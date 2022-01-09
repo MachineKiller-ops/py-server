edc23d11abbe928609105ffdb8d0d2ce1c1d5053
Caio Cesar <caiocesaraguilar14@hotmail.com>
2022-01-09 16:26:37 -0300

    After request

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_from_directory
+from flask import jsonify
 
 app = Flask(__name__, static_url_path='')
 
@@ -8,3 +9,11 @@ def index():
 
     file_name = 'SM.xlsx'
     return app.send_static_file(filename=file_name)
+
+
+@app.after_request
+def set_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+    return response
