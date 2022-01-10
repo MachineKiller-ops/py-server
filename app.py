a9360491fb942135469ce68eccdb93e043027d4d
Caio Cesar <caiocesaraguilar14@hotmail.com>
2022-01-10 14:45:45 -0300

    Grava SM no arquivo excel

--- app.py
+++ app.py
@@ -7,24 +7,26 @@ app = Flask(__name__, static_url_path='')
 
 
 def print_conteudo(event_data):
-    print(event_data.get("body"))
+    print(event_data[0])
+    # load excel file
+    workbook = load_workbook(filename=os.path.join('static', "SM.xlsx"))
+    # open workbook
+    sheet = workbook.active
+    # modify the desired cell
+    print(len(event_data))
+    for i, new_val in enumerate(event_data):
+        num = i+8
+        indice = "C%d" % num
+        sheet[indice] = new_val
+        print(new_val)
+    # save the file
+    workbook.save(filename=os.path.join('static', "SMout.xlsx"))
 
 
 @app.route("/", methods=['POST'])
 def index():
     event_data = request.json
     print_conteudo(event_data)
-    # load excel file
-    workbook = load_workbook(filename=os.path.join('static', "SM.xlsx"))
-
-    # open workbook
-    sheet = workbook.active
-
-    # modify the desired cell
-    sheet["H3"] = "SE ATALAIA"
-
-    # save the file
-    workbook.save(filename=os.path.join('static', "SMout.xlsx"))
 
     file_name = 'SMout.xlsx'
     return app.send_static_file(filename=file_name)
@@ -45,4 +47,4 @@ def set_headers(response):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5005)
+    app.run(host='127.0.0.1', port=5006)
