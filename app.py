192af706e16fd6431f94ccf09bfa1991a5659699
Caio Cesar <caiocesaraguilar14@hotmail.com>
2022-01-13 21:05:54 -0300

    Adicionada função que cria uma nova linha quando o item de manobra é muito longo

--- app.py
+++ app.py
@@ -2,14 +2,42 @@ from flask import Flask, request
 from flask import json
 from openpyxl import load_workbook
 import os
+import textwrap
 
 app = Flask(__name__, static_url_path='')
 
 
-def cria_pagina(event_data, page, cont, tipo):
+def organiza_dados(seqman):
+    seqIndice = []
+    sequencia = []
+    cont = enumerate(seqman)
+    for i, item in enumerate(seqman):
+        wrapper = textwrap.TextWrapper(width=80)
+        lista = wrapper.wrap(text=item)
+        # del seqman[i]
+        # print(lista)
+        sequencia.extend(lista)
+        seqIndice.append(i)
+        j = 1
+        while (j < len(lista)):
+            seqIndice.append('XXX')
+            j += 1
+        """ #seqindice[i] = i+1
+        j = 1
+        while (j < len(lista)):
+            i += 1
+            j += 1
+            # seqindice[i] = 'XXX'
+            cont += 1 """
+    print(seqIndice, sequencia)
+    return seqIndice, sequencia
+
+
+def cria_pagina(event_data, page, cont, tipo, seqIndice):
     if(page > 0):
         # Se não for a primeira página deleta os 32 primeiros itens, pois os mesmo já foram escritos
         del event_data[0:32]
+        del seqIndice[0:32]
     # load excel file
     workbook = load_workbook(filename=os.path.join(
         'static', "SMout.xlsx"))  # Carrega o arquivo de saída
@@ -24,13 +52,15 @@ def cria_pagina(event_data, page, cont, tipo):
         cell_time = "B%d" % num
         cell_im = "C%d" % num
         sheet[cell_im] = new_val
-        sheet[cell_ix] = cont
+        sheet[cell_ix] = seqIndice[i]
         sheet[cell_time] = ':'
+        if(seqIndice[i] == 'XXX'):
+            sheet[cell_time] = 'XXX'
+            print('Entrooooooooooooooooooooooooou')
         cont += 1
-        print(i)
         if (i >= 31):  # Caso a SM tenha mais de 32 itens, chama-se a função de maneira recursiva para escrever na próxima página
             workbook.save(filename=os.path.join('static', "SMout.xlsx"))
-            page = cria_pagina(event_data, page, cont, tipo)
+            page = cria_pagina(event_data, page, cont, tipo, seqIndice)
             return page
         # Caso se chega ao fim do Array da SM, atribui-se os XXXXX a próxima linha CASO o espaço não tenha acabado
         elif(i == len(event_data)-1):
@@ -61,12 +91,16 @@ def index():
         'static', "SM.xlsx"))  # Carrega o template original
     # Salva em um arquivo de saída
     workbook.save(filename=os.path.join('static', "SMout.xlsx"))
-    #workbook = load_workbook(filename=os.path.join('static', "SMout.xlsx"))
+    # workbook = load_workbook(filename=os.path.join('static', "SMout.xlsx"))
+    seqIndice, seqIsolar = organiza_dados(event_data['seqIsolar'])
+    print('tamanho inidice' + str(len(seqIndice)) +
+          'tamanho seq ' + str(len(seqIsolar)))
     # Escreve nos sheets de ISOLAR
-    cria_pagina(event_data['seqIsolar'], 0, 1, "ISOLAR-")
+    cria_pagina(seqIsolar, 0, 1, "ISOLAR-", seqIndice)
     print(event_data['seqNormalizar'])
+    seqIndice, seqNormalizar = organiza_dados(event_data['seqNormalizar'])
     # Escreve nos sheets de normalizar
-    cria_pagina(event_data['seqNormalizar'], 0, 1, "NORMALIZAR-")
+    cria_pagina(seqNormalizar, 0, 1, "NORMALIZAR-", seqIndice)
 
     file_name = 'SMout.xlsx'
     return app.send_static_file(filename=file_name)
